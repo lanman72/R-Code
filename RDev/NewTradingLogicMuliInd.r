fda826c7af5045ed1b94637e9ff1e47c556781a6
lanman72 <landolph@sbcglobal.net>
2017-03-24 17:47:05 -0500

    Add files via upload

--- RDev/NewTradingLogicMuliInd.r
+++ RDev/NewTradingLogicMuliInd.r
@@ -17,6 +17,32 @@ datUUP <- getsdat('UUP', 1000)
 datFAS <- getsdat('FAS', 1000)
 datVIX <- getsdat('^VIX', 1000)
 
+datGLD["2017-03/", c('Open', 'High', 'Low', 'Close')]
+data.frame(rollapply(datGLD$High,20,FUN = "max") - rollapply(datGLD$High,14,FUN = "max"), 
+rollapply(datGLD$Low,20,FUN = "min") - rollapply(datGLD$Low,14,FUN = "min"),
+(datGLD$High+datGLD$Low)/2,
+rollapply(datGLD$Close, 2, FUN = "mean"),
+((datGLD$High+datGLD$Low)/2) / rollapply(datGLD$Open+datGLD$Close, 2, FUN = "mean"),
+datGLD$High, datGLD$Low)
+
+rollapply(100 * (datGLD$Close - rollapply(datGLD$Low,14,FUN = "min")) / (rollapply(datGLD$High,14,FUN = "max") - rollapply(datGLD$Low,14,FUN = "min") ), 3,"mean") -
+rollapply(rollapply(100 * (datGLD$Close - rollapply(datGLD$Low,14,FUN = "min")) / (rollapply(datGLD$High,14,FUN = "max") - rollapply(datGLD$Low,14,FUN = "min") ), 3,"mean"), 3, "mean")
+
+n = 14*3*5
+
+lan <- data.frame(n,
+rollapply(datGLD$Low,n,FUN = "min") + ((rollapply(datGLD$High,n,FUN = "max") - rollapply(datGLD$Low,n,FUN = "min")) * .75), 
+rollapply(datGLD$Low,n,FUN = "min") + ((rollapply(datGLD$High,n,FUN = "max") - rollapply(datGLD$Low,n,FUN = "min")) * .25), 
+datGLD$Low, datGLD$Close, datGLD$Close - Lag(datGLD$Close))
+
+
+rollapply(datGLD$Low,n,FUN = "min") + ((rollapply(datGLD$High,n,FUN = "max") - rollapply(datGLD$Low,n,FUN = "min")) * .80)
+
+rollmean(datGLD$High, 5) - rollmean(datGLD$High, 14)   
+rollmean(datGLD$Low, 5) - rollmean(datGLD$Low, 14)   
+rollSFM(datSP500$Close, datTLT$Close, 14)
+
+
 
 datSP500[datVIX$Longsig != 0, c("Open", "Close") ]
 
@@ -52,5 +78,6 @@ for (ticker in row.names(tdata) ) {
 
 mtext(chartHdr, line=2, adj=.5, cex=.5, col="black")
 sdat <- datFAS
+
 
 
